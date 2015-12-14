b33fe2a3f887c8eb3a027c9ac8bd9d4d845d30fb
Salman Akram <salmanit2012@gmail.com>
2015-12-14 17:20:53 -0500

    adjust methods and headers in redirection

--- README.md
+++
@@ -1,1 +0,0 @@
-Create a simple Dynamic Site with Node that display student's profile information by creating a server. 
--- router.js
+++ router.js
@@ -1,51 +1,87 @@
 var Profile = require("./profile.js");
+var renderer = require("./renderer.js");
+var querystring = require("querystring");
 
-// Handle HTTP route GET / and POST /
+var commonHeaders = {'Content-Type': 'text/html'};
+
+//Handle HTTP route GET / and POST / i.e. Home
 function home(request, response) {
+  //if url == "/" && GET
   if(request.url === "/") {
-    // Show search
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.write("Header\n");
-    response.write("Search\n");
-    response.end("Footer\n");
+    if(request.method.toLowerCase() === "get") {
+      //show search
+      response.writeHead(200, commonHeaders);  
+      renderer.view("header", {}, response);
+      renderer.view("search", {}, response);
+      renderer.view("footer", {}, response);
+      response.end();
+    } else {
+      //if url == "/" && POST
+      
+      //get the post data from body
+      request.on("data", function(postBody) {
+        //extract the username
+        var query = querystring.parse(postBody.toString());
+        response.write(query.username);
+        response.end();
+        //redirect to /:username
+      });
+      
+    }
   }
+  
 }
 
-// Handle HTTP route GET /:username
+//Handle HTTP route GET /:username i.e. /chalkers
 function user(request, response) {
+  //if url == "/...."
   var username = request.url.replace("/", "");
   if(username.length > 0) {
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.write("Header\n");
-
-      //get JSON from Treehouse
-      var studentProfile = new Profile(username);
-      // on "end"
-      studentProfile.on("end", function(profileJSON){
-      // show profile
-
-      // store the values which we need
+    response.writeHead(200, commonHeaders);  
+    renderer.view("header", {}, response);    
+    
+    //get json from Treehouse
+    var studentProfile = new Profile(username);
+    //on "end"
+    studentProfile.on("end", function(profileJSON){
+      //show profile
+      
+      //Store the values which we need
       var values = {
-        avatarUrl: profileJSON.gravatar_url,
+        avatarUrl: profileJSON.gravatar_url, 
         username: profileJSON.profile_name,
-        badges: profileJSOn.badges.length,
+        badges: profileJSON.badges.length,
         javascriptPoints: profileJSON.points.JavaScript
       }
-
-      // Simple response
-        response.write(values.username + " has " + values.badges + " badges\n");
-        response.end("Footer\n");
+      //Simple response
+      renderer.view("profile", values, response);
+      renderer.view("footer", {}, response);
+      response.end();
     });
-
-    // on "error"
+        
+    //on "error"
     studentProfile.on("error", function(error){
-    // show error
-    response.write(error.message + "\n");
-    response.end('Footer\n');
+      //show error
+      renderer.view("error", {errorMessage: error.message}, response);
+      renderer.view("search", {}, response);
+      renderer.view("footer", {}, response);
+      response.end();
     });
-
+      
   }
 }
 
 module.exports.home = home;
 module.exports.user = user;
+
+
+
+
+
+
+
+
+
+
+
+
